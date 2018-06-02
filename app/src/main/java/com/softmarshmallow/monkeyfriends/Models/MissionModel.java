55eb53742f13cd845245ef2ba37cd04f7a00f05f
izen1231 <izen1231@gmail.com>
2018-06-03 06:50:00 +0900

    fix

---
+++ app/src/main/java/com/softmarshmallow/monkeyfriends/Models/MissionModel.java
@@ -0,0 +1,9 @@
+package com.softmarshmallow.monkeyfriends.Models;
+
+class MissionModel {
+    public User doer;
+    public User giver;
+    public String giverComment;
+    public String missionStarted;
+    public Boolean isMissionCompleted;
+}

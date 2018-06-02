2610bd60f72475bde55631ed669d245659e5f16c
izen1231 <izen1231@gmail.com>
2018-06-02 22:59:50 +0900

    change

--- app/src/main/java/com/softmarshmallow/monkeyfriends/Views/ExtraOptionsView/ExtraOptionsViewFragment.java
+++ app/src/main/java/com/softmarshmallow/monkeyfriends/Views/ExtraOptionsView/ExtraOptionsViewFragment.java
@@ -7,25 +7,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import com.softmarshmallow.monkeyfriends.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ExtraOptionsViewFragment extends Fragment
 {
-        
-        
+
         public ExtraOptionsViewFragment() {
                 // Required empty public constructor
         }
-        
-        
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+
                 // Inflate the layout for this fragment
                 return inflater.inflate(R.layout.fragment_extra_options_view, container, false);
         }
-        
+
 }
+

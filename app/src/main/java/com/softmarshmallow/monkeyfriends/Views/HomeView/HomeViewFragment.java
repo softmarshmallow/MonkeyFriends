5d70a12095113ec66891d4e54d5800928f07a052
woojoo <woojoo@softmarshmallow.com>
2018-06-03 10:22:04 +0900

    buttons

--- app/src/main/java/com/softmarshmallow/monkeyfriends/Views/HomeView/HomeViewFragment.java
+++ app/src/main/java/com/softmarshmallow/monkeyfriends/Views/HomeView/HomeViewFragment.java
@@ -3,11 +3,15 @@ package com.softmarshmallow.monkeyfriends.Views.HomeView;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.softmarshmallow.monkeyfriends.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,7 +29,35 @@ public class HomeViewFragment extends Fragment
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
                 // Inflate the layout for this fragment
-                return inflater.inflate(R.layout.fragment_home_view, container, false);
+                View view =  inflater.inflate(R.layout.fragment_home_view, container, false);
+                
+                ButterKnife.bind(view);
+                
+                return view;
         }
+        
+        @OnClick(R.id.logButton)
+        void OnLogButtonClick(){
+                Log.d("W", "WORDKSSS");
+        }
+        
+        
+        @OnClick(R.id.badgeButton)
+        void OnBadgeButtonClick(){
+        
+        }
+        
+        @OnClick(R.id.newMissionButton)
+        void OnNewMissionButtonClick(){
+        
+        }
+        
+        @OnClick(R.id.todayMissionButton)
+        void OnTodayMissionButtonClick(){
+        
+        }
+        
+        
+        
         
 }
